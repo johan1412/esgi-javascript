370c455b891d7cfea6cdede0cabff52c42d52dfb
johan <johan2.github@gmail.com>
2020-11-30 15:07:50 +0100

    exercice 4

--- exercise-4/mario-kart.js
+++ exercise-4/mario-kart.js
@@ -1,6 +1,8 @@
 function Pilote(name) {
+    this.name = name;
     const props = {};
     let prevProps = {};
+
 
     this.receiveData = function (data) {
         prevProps = Object.assign({}, props);
@@ -9,13 +11,16 @@ function Pilote(name) {
         props.position = data.position;
     };
 
+
     this.needUpdate = function () {
         return JSON.stringify(prevProps) !== JSON.stringify(props);
     }
 
+
     this.getState = function () {
         return props.state;
     }
+
 
     this.speak = function () {
         switch (props.state) {
@@ -40,4 +45,80 @@ function Pilote(name) {
                 break;
         }
     }
+
+}
+
+
+
+function Vehicule() {
+    const pilote;
+    this.number;
+    const props = {};
+    const prevProps = {};
+    let onFire = () => {};
+
+
+    this.init = function (conf) {
+        pilote = new Pilote(conf.name);
+        this.number = conf.number;
+        props.weapon = conf.weapon;
+        props.maxVelocity = conf.maxVelocity;
+        props.distance = 0;
+        onFire = conf.onFire;
+
+        prevProps = Object.assign({}, props);
+    }
+
+
+    this.receiveData = function (data) {
+        prevProps = Object.assign({}, props);
+
+        props.event = data.event;
+        props.value = Object.assign({}, data.value);
+        props.competitors = Object.assign({}, data.competitors);
+        props.distance = data.distance;
+    }
+
+
+    this.needUpdate = function () {
+        return JSON.stringify(prevProps) !== JSON.stringify(props);
+    }
+
+
+    let ride = function (x) {
+        props.distance += x;
+
+    }
+
+
+    let setWeapon = function (weapon) {
+        props.weapon = weapon;
+        pilote.receiveData({state: "happy"});
+        ride(props.maxVelocity);
+    }
+
+    
+    let fireWeapon = function () {
+        ride(props.maxVelocity);
+
+    }
+
+
+    let touched = function (data) {
+        if(props.weapon === "bubble") {
+            ride(props.maxVelocity);
+        } else {
+            ride(data.effect);
+        }
+        pilote.receiveData({state: "sad", origin: data.origin});
+    }
+
+
+    this.display = function () {
+        console.log("distance: " + props.distance);
+        console.log("arme: " + props.weapon);
+        console.log("pilote: " + JSON.stringify(pilote));
+        console.log("vitesse: " + props.maxVelocity);
+    }
+
 }
